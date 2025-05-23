2227661d174005126cd3f6523fdddf19b6a456b0
kabandaarthur12 <nanfukaannet308@gmail.com>
2025-05-23 11:11:04 +0300

    inventory management system

---
+++ error_handling.py
@@ -0,0 +1,16 @@
+while True:
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        result = num1 / num2
+
+    except ValueError:
+        print("Invalid input! Please enter numeric values.")
+        continue
+    
+    except ZeroDivisionError:
+        print("Cannot divide by zero! Please enter a non-zero second number.")
+        continue
+    else:
+        print(f"The result is: {result}")
+        break
